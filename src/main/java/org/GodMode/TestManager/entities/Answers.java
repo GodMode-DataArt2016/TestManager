f14f011096cbed339bea9947957c141d4897dc7b
Oleg <zinoff1979@gmail.com>
2016-05-01 23:51:40 +0300

    add implementation of Answers entity

--- src/main/java/org/GodMode/TestManager/entities/Answers.java
+++ src/main/java/org/GodMode/TestManager/entities/Answers.java
@@ -1,7 +1,70 @@
 package org.GodMode.TestManager.entities;
 
+import javax.persistence.*;
+
 /**
  * Created by Oleg on 01.05.2016.
  */
+
+@Entity
+@Table(name = "Answers")
 public class Answers {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "a_id")
+    private Long aId;
+
+    @ManyToOne
+    @JoinColumn(name = "q_id")
+    private Questions questions;
+
+    @Column(name = "text")
+    private String text;
+
+    @Column(name = "is_right")
+    private boolean isRight;
+
+    @Column(name = "pic_src")
+    private Long picSrc;
+
+    public Long getaId() {
+        return aId;
+    }
+
+    public void setaId(Long aId) {
+        this.aId = aId;
+    }
+
+    public Questions getQuestions() {
+        return questions;
+    }
+
+    public void setQuestions(Questions questions) {
+        this.questions = questions;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean isRight() {
+        return isRight;
+    }
+
+    public void setIsRight(boolean isRight) {
+        this.isRight = isRight;
+    }
+
+    public Long getPicSrc() {
+        return picSrc;
+    }
+
+    public void setPicSrc(Long picSrc) {
+        this.picSrc = picSrc;
+    }
 }
