f2b2a1ba01a20bcefe79f5f43682d195f72b45d0
Oleg <zinoff1979@gmail.com>
2016-05-01 23:25:59 +0300

    add empty entities and class mapping in hibernate config

---
+++ src/main/java/org/GodMode/TestManager/entities/Block.java
@@ -0,0 +1,7 @@
+package org.GodMode.TestManager.entities;
+
+/**
+ * Created by Oleg on 01.05.2016.
+ */
+public class Block {
+}
--- src/main/java/org/GodMode/TestManager/entities/Empty.java
+++
@@ -1,7 +0,0 @@
-package org.GodMode.TestManager.entities;
-
-/**
- * Created by ozinoviev on 4/26/2016.
- */
-public class Empty {
-}
