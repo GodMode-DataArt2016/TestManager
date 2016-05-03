a6d1a3de25fdfe9efa631fc5def62b56b1085132
Nataliia Dziubenko <smthelusive@gmail.com>
2016-05-04 00:22:24 +0300

    completed tasks 2, 7, 12

--- src/main/java/org/GodMode/TestManager/entities/Block.java
+++ src/main/java/org/GodMode/TestManager/entities/Block.java
@@ -1,7 +1,56 @@
 package org.GodMode.TestManager.entities;
 
-/**
- * Created by Oleg on 01.05.2016.
- */
+import org.hibernate.mapping.Set;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "Block")
 public class Block {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "b_id")
+    private Long bId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @OneToMany(mappedBy = "TestsBlocks")
+    private Set testsBlocks;
+
+    public Long getbId() {
+        return bId;
+    }
+
+    public void setbId(Long bId) {
+        this.bId = bId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Set getTestsBlocks() {
+        return testsBlocks;
+    }
+
+    public void setTestsBlocks(Set testsBlocks) {
+        this.testsBlocks = testsBlocks;
+    }
 }
