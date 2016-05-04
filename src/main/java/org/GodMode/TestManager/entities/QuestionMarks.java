42a7e99ade87965d256c546757362d9f699e2576
avoevodin81 <avoevodin81@gmail.com>
2016-05-04 12:50:52 +0300

    Update QuestionMarks.java (#18)
    * Update QuestionMarks.java
    
    * Update QuestionMarks.java
    
    * Update QuestionMarks.java

--- src/main/java/org/GodMode/TestManager/entities/QuestionMarks.java
+++ src/main/java/org/GodMode/TestManager/entities/QuestionMarks.java
@@ -3,5 +3,60 @@ package org.GodMode.TestManager.entities;
 /**
  * Created by Oleg on 01.05.2016.
  */
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "QuestionMarks")
 public class QuestionMarks {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "gm_id")
+    private Long gmId;
+
+    @ManyToOne
+    @JoinColumn(name = "u_id")
+    private Users users;
+
+    @ManyToOne
+    @JoinColumn(name = "q_id")
+    private Question question;
+
+    @Column(name = "is_right")
+    private Boolean isRight;
+
+    public Long getGmId() {
+        return gmId;
+    }
+
+    public void setGmId(Long gmId) {
+        this.gmId = gmId;
+    }
+
+    public Users getUsers() {
+        return users;
+    }
+
+    public void setUsers(Users users) {
+        this.users = users;
+    }
+
+    public Question getQuestion() {
+        return question;
+    }
+
+    public void setQuestion(Question question) {
+        this.question = question;
+    }
+
+    public Boolean getRight() {
+        return isRight;
+    }
+
+    public void setRight(Boolean right) {
+        isRight = right;
+    }
 }
+
+
