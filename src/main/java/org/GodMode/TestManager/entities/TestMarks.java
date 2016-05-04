d1ad108170aaa501c6142f23c9147d5647d0bb4a
smthelusive <smthelusive@gmail.com>
2016-05-04 09:43:02 +0300

    completed tasks 2, 7, 12 (#17)

--- src/main/java/org/GodMode/TestManager/entities/TestMarks.java
+++ src/main/java/org/GodMode/TestManager/entities/TestMarks.java
@@ -1,7 +1,34 @@
 package org.GodMode.TestManager.entities;
 
-/**
- * Created by Oleg on 01.05.2016.
- */
+import javax.persistence.*;
+
+@Entity
+@Table(name = "TestMarks")
 public class TestMarks {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "tm_id")
+    private Long tmId;
+
+    @Column(name = "mark")
+    private Long mark;
+
+
+    public Long getTmId() {
+        return tmId;
+    }
+
+    public void setTmId(Long tmId) {
+        this.tmId = tmId;
+    }
+
+    public Long getMark() {
+        return mark;
+    }
+
+    public void setMark(Long mark) {
+        this.mark = mark;
+    }
+
 }
