cee7fad3f41d12f31133e3f947ebbe294042407d
avoevodin81 <avoevodin81@gmail.com>
2016-05-10 17:32:04 +0300

    Add files via upload

--- src/main/java/org/GodMode/TestManager/service/impl/BlockServiceImpl.java
+++ src/main/java/org/GodMode/TestManager/service/impl/BlockServiceImpl.java
@@ -1,1 +1,29 @@
+package org.GodMode.TestManager.service.impl;
 
+import org.GodMode.TestManager.entities.Block;
+import org.GodMode.TestManager.service.BlockService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+
+public class BlockServiceImpl implements BlockService {
+    @Autowired
+    BlockServiceImpl blockServiceDao;
+
+    public List<Block> findAll() {
+        return blockServiceDao.findAll();
+    }
+
+    public Block find(Long id) {
+        return blockServiceDao.find(id);
+    }
+
+    public void saveOrUpdate(Block entry) {
+        blockServiceDao.saveOrUpdate(entry);
+    }
+
+    public void delete(Block entry) {
+        blockServiceDao.delete(entry);
+    }
+}
